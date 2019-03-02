927d6b50897cc94f53a23cfdeee904c593824e85
Sofia Vistas <sofia.vistas@gmail.com>
2019-03-02 16:15:49 +0100

    Add data, api and helpers

---
+++ would-you-rather/src/utils/helpers.js
@@ -0,0 +1,26 @@
+export function generateUID () {
+  return (
+    Math.random()
+      .toString(36)
+      .substring(2, 15) +
+    Math.random()
+      .toString(36)
+      .substring(2, 15)
+  )
+}
+
+export function formatQuestion ({ firstText, optionTwoText, author }) {
+  return {
+    id: generateUID(),
+    timestamp: Date.now(),
+    author,
+    optionOne: {
+      votes: [],
+      text: optionOneText
+    },
+    optionTwo: {
+      votes: [],
+      text: optionTwoText
+    }
+  }
+}
